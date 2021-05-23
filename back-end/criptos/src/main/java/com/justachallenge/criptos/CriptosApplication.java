eae586741834f094becbe72b9f165c940fee8f62
Hugo Souza <hugoregos@gmail.com>
2021-05-23 18:55:16 -0300

    Projeto Spring criado + criação dos pacotes básicos

---
+++ back-end/criptos/src/main/java/com/justachallenge/criptos/CriptosApplication.java
@@ -0,0 +1,13 @@
+package com.justachallenge.criptos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptosApplication.class, args);
+	}
+
+}
---
+++ back-end/criptos/src/test/java/com/justachallenge/criptos/CriptosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justachallenge.criptos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
